81cf654507ff4ed6239ef713c58c05a467fd0ca9
Kevin Malakoff <kmalakoff@gmail.com>
2020-05-24 11:18:56 -0700

    Handle strip correctly. Added graceful-fs

--- test/spec/api.test.js
+++ test/spec/api.test.js
@@ -16,40 +16,68 @@ describe('api', function () {
     });
   });
 
-  it('extract file with progress', function (done) {
-    var progressUpdates = [];
-    function progress(update) {
-      progressUpdates.push(update);
-    }
+  describe('happy path', function () {
+    it('extract file with progress', function (done) {
+      var progressUpdates = [];
+      function progress(update) {
+        progressUpdates.push(update);
+      }
 
-    extract(path.join(DATA_DIR, 'fixture.js'), TMP_DIR, { progress: progress }, function (err) {
-      assert.ok(!err);
-
-      fs.readdir(TMP_DIR, function (err, files) {
+      extract(path.join(DATA_DIR, 'fixture.js'), TMP_DIR, { progress: progress }, function (err) {
         assert.ok(!err);
-        assert.equal(files.length, 1);
-        assert.equal(files[0], 'fixture.js');
-        assert.ok(progressUpdates.length === 1);
-        done();
+
+        fs.readdir(TMP_DIR, function (err, files) {
+          assert.ok(!err);
+          assert.equal(files.length, 1);
+          assert.equal(files[0], 'fixture.js');
+          assert.ok(progressUpdates.length === 1);
+          done();
+        });
+      });
+    });
+
+    it('extract tar with progress', function (done) {
+      var progressUpdates = [];
+
+      function progress(update) {
+        progressUpdates.push(update);
+      }
+
+      extract(path.join(DATA_DIR, 'fixture.tar'), TMP_DIR, { strip: 1, progress: progress }, function (err) {
+        assert.ok(!err);
+
+        fs.readdir(TMP_DIR, function (err, files) {
+          assert.ok(!err);
+          assert.deepEqual(files.sort(), ['file.txt', 'link']);
+          assert.equal(fs.realpathSync(path.join(TMP_DIR, 'link')), path.join(TMP_DIR, 'file.txt'));
+          assert.ok(progressUpdates.length === 3);
+          done();
+        });
+      });
+    });
+
+    it('extract multiple times', function (done) {
+      extract(path.join(DATA_DIR, 'fixture.tar'), TMP_DIR, { strip: 1 }, function (err) {
+        assert.ok(!err);
+
+        extract(path.join(DATA_DIR, 'fixture.tar'), TMP_DIR, { strip: 1 }, function (err) {
+          assert.ok(!err);
+
+          fs.readdir(TMP_DIR, function (err, files) {
+            assert.ok(!err);
+            assert.deepEqual(files.sort(), ['file.txt', 'link']);
+            assert.equal(fs.realpathSync(path.join(TMP_DIR, 'link')), path.join(TMP_DIR, 'file.txt'));
+            done();
+          });
+        });
       });
     });
   });
 
-  it('extract tar with progress', function (done) {
-    var progressUpdates = [];
-
-    function progress(update) {
-      progressUpdates.push(update);
-    }
-
-    extract(path.join(DATA_DIR, 'fixture.tar'), TMP_DIR, { strip: 1, progress: progress }, function (err) {
-      assert.ok(!err);
-
-      fs.readdir(TMP_DIR, function (err, files) {
-        assert.ok(!err);
-        assert.deepEqual(files.sort(), ['file.txt', 'link']);
-        assert.equal(fs.realpathSync(path.join(TMP_DIR, 'link')), path.join(TMP_DIR, 'file.txt'));
-        assert.ok(progressUpdates.length === 3);
+  describe('unhappy path', function () {
+    it('should fail with too large strip', function (done) {
+      extract(path.join(DATA_DIR, 'fixture.tar'), TMP_DIR, { strip: 2 }, function (err) {
+        assert.ok(err);
         done();
       });
     });
