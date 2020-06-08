7e43d995e197a2d24057c040dcae94d392b8e9ee
Kevin Malakoff <kmalakoff@gmail.com>
2020-06-07 20:45:53 -0700

    Got rename of files working

--- test/spec/extract-callback.test.js
+++ test/spec/extract-callback.test.js
@@ -8,12 +8,14 @@ var semver = require('semver');
 var extract = require('../..');
 
 var TMP_DIR = path.resolve(path.join(__dirname, '..', '..', '.tmp'));
+var TARGET = path.resolve(path.join(TMP_DIR, 'target'));
 var DATA_DIR = path.resolve(path.join(__dirname, '..', 'data'));
 
-describe('api', function () {
-  beforeEach(function (done) {
-    rimraf(TMP_DIR, function () {
-      mkpath(TMP_DIR, done);
+describe('extract-callback', function () {
+  beforeEach(function (callback) {
+    rimraf(TMP_DIR, function (err) {
+      if (err && err.code !== 'EEXIST') return callback(err);
+      mkpath(TMP_DIR, callback);
     });
   });
 
@@ -24,14 +26,14 @@ describe('api', function () {
         progressUpdates.push(update);
       }
 
-      extract(path.join(DATA_DIR, 'fixture.js'), TMP_DIR, { progress: progress }, function (err) {
+      extract(path.join(DATA_DIR, 'fixture.js'), TARGET, { progress: progress }, function (err) {
         assert.ok(!err);
 
-        fs.readdir(TMP_DIR, function (err, files) {
+        fs.readdir(TARGET, function (err, files) {
           assert.ok(!err);
           assert.equal(files.length, 1);
           assert.equal(files[0], 'fixture.js');
-          assert.ok(progressUpdates.length === 1);
+          assert.ok(progressUpdates.length > 0);
           done();
         });
       });
@@ -44,13 +46,13 @@ describe('api', function () {
         progressUpdates.push(update);
       }
 
-      extract(path.join(DATA_DIR, 'fixture.tar'), TMP_DIR, { strip: 1, progress: progress }, function (err) {
+      extract(path.join(DATA_DIR, 'fixture.tar'), TARGET, { strip: 1, progress: progress }, function (err) {
         assert.ok(!err);
 
-        fs.readdir(TMP_DIR, function (err, files) {
+        fs.readdir(TARGET, function (err, files) {
           assert.ok(!err);
           assert.deepEqual(files.sort(), ['file.txt', 'link']);
-          assert.equal(fs.realpathSync(path.join(TMP_DIR, 'link')), path.join(TMP_DIR, 'file.txt'));
+          assert.equal(fs.realpathSync(path.join(TARGET, 'link')), path.join(TARGET, 'file.txt'));
           assert.equal(progressUpdates.length, 3);
           done();
         });
@@ -58,16 +60,16 @@ describe('api', function () {
     });
 
     it('extract tar multiple times', function (done) {
-      extract(path.join(DATA_DIR, 'fixture.tar'), TMP_DIR, { strip: 1 }, function (err) {
+      extract(path.join(DATA_DIR, 'fixture.tar'), TARGET, { strip: 1 }, function (err) {
         assert.ok(!err);
 
-        extract(path.join(DATA_DIR, 'fixture.tar'), TMP_DIR, { strip: 1 }, function (err) {
+        extract(path.join(DATA_DIR, 'fixture.tar'), TARGET, { strip: 1 }, function (err) {
           assert.ok(!err);
 
-          fs.readdir(TMP_DIR, function (err, files) {
+          fs.readdir(TARGET, function (err, files) {
             assert.ok(!err);
             assert.deepEqual(files.sort(), ['file.txt', 'link']);
-            assert.equal(fs.realpathSync(path.join(TMP_DIR, 'link')), path.join(TMP_DIR, 'file.txt'));
+            assert.equal(fs.realpathSync(path.join(TARGET, 'link')), path.join(TARGET, 'file.txt'));
             done();
           });
         });
@@ -83,13 +85,13 @@ describe('api', function () {
         progressUpdates.push(update);
       }
 
-      extract(path.join(DATA_DIR, 'fixture.tar'), TMP_DIR, { strip: 1, progress: progress }, function (err) {
+      extract(path.join(DATA_DIR, 'fixture.tar'), TARGET, { strip: 1, progress: progress }, function (err) {
         assert.ok(!err);
 
-        fs.readdir(TMP_DIR, function (err, files) {
+        fs.readdir(TARGET, function (err, files) {
           assert.ok(!err);
           assert.deepEqual(files.sort(), ['file.txt', 'link']);
-          assert.equal(fs.realpathSync(path.join(TMP_DIR, 'link')), path.join(TMP_DIR, 'file.txt'));
+          assert.equal(fs.realpathSync(path.join(TARGET, 'link')), path.join(TARGET, 'file.txt'));
           assert.equal(progressUpdates.length, 3);
           done();
         });
@@ -99,16 +101,16 @@ describe('api', function () {
     it('extract zip multiple times', function (done) {
       if (semver.lt(process.versions.node, '0.9.0')) return done(); // yauzl does not read the master record properly on Node 0.8
 
-      extract(path.join(DATA_DIR, 'fixture.tar'), TMP_DIR, { strip: 1 }, function (err) {
+      extract(path.join(DATA_DIR, 'fixture.zip'), TARGET, { strip: 1 }, function (err) {
         assert.ok(!err);
 
-        extract(path.join(DATA_DIR, 'fixture.tar'), TMP_DIR, { strip: 1 }, function (err) {
+        extract(path.join(DATA_DIR, 'fixture.zip'), TARGET, { strip: 1 }, function (err) {
           assert.ok(!err);
 
-          fs.readdir(TMP_DIR, function (err, files) {
+          fs.readdir(TARGET, function (err, files) {
             assert.ok(!err);
             assert.deepEqual(files.sort(), ['file.txt', 'link']);
-            assert.equal(fs.realpathSync(path.join(TMP_DIR, 'link')), path.join(TMP_DIR, 'file.txt'));
+            assert.equal(fs.realpathSync(path.join(TARGET, 'link')), path.join(TARGET, 'file.txt'));
             done();
           });
         });
@@ -118,7 +120,7 @@ describe('api', function () {
 
   describe('unhappy path', function () {
     it('should fail with too large strip (tar)', function (done) {
-      extract(path.join(DATA_DIR, 'fixture.tar'), TMP_DIR, { strip: 2 }, function (err) {
+      extract(path.join(DATA_DIR, 'fixture.tar'), TARGET, { strip: 2 }, function (err) {
         assert.ok(!!err);
         done();
       });
@@ -127,7 +129,7 @@ describe('api', function () {
     it('should fail with too large strip (zip)', function (done) {
       if (semver.lt(process.versions.node, '0.9.0')) return done(); // yauzl does not read the master record properly on Node 0.8
 
-      extract(path.join(DATA_DIR, 'fixture.zip'), TMP_DIR, { strip: 2 }, function (err) {
+      extract(path.join(DATA_DIR, 'fixture.zip'), TARGET, { strip: 2 }, function (err) {
         assert.ok(!!err);
         done();
       });
