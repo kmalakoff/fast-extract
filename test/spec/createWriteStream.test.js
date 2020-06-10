4db0289c62b068615821f7eb7a8505bc80841640
Kevin Malakoff <kmalakoff@gmail.com>
2020-06-09 17:08:12 -0700

    Added tests for write streams

--- test/spec/createWriteStream.test.js
+++ test/spec/createWriteStream.test.js
@@ -6,12 +6,13 @@ var mkpath = require('mkpath');
 var semver = require('semver');
 
 var extract = require('../..');
+var createWriteStream = require('../..').createWriteStream;
 
 var TMP_DIR = path.resolve(path.join(__dirname, '..', '..', '.tmp'));
 var TARGET = path.resolve(path.join(TMP_DIR, 'target'));
 var DATA_DIR = path.resolve(path.join(__dirname, '..', 'data'));
 
-describe('extract', function () {
+describe('createWriteStream', function () {
   beforeEach(function (callback) {
     rimraf(TMP_DIR, function (err) {
       if (err && err.code !== 'EEXIST') return callback(err);
@@ -26,9 +27,11 @@ describe('extract', function () {
         progressUpdates.push(update);
       }
 
-      extract(path.join(DATA_DIR, 'fixture.js'), TARGET, { progress: progress }, function (err) {
+      var res = fs.createReadStream(path.join(DATA_DIR, 'fixture.js')).pipe(createWriteStream(TARGET, { basename: 'fixture.js', progress: progress }));
+      res.on('error', function (err) {
         assert.ok(!err);
-
+      });
+      res.on('finish', function () {
         fs.readdir(TARGET, function (err, files) {
           assert.ok(!err);
           assert.equal(files.length, 1);
@@ -39,24 +42,25 @@ describe('extract', function () {
       });
     });
 
-    it('extract file with progress - promise', function (done) {
-      if (typeof Promise === 'undefined') return done();
+    it('extract file with progress - no basename', function (done) {
       var progressUpdates = [];
       function progress(update) {
         progressUpdates.push(update);
       }
 
-      extract(path.join(DATA_DIR, 'fixture.js'), TARGET, { progress: progress })
-        .then(function () {
-          fs.readdir(TARGET, function (err, files) {
-            assert.ok(!err);
-            assert.equal(files.length, 1);
-            assert.equal(files[0], 'fixture.js');
-            assert.ok(progressUpdates.length > 0);
-            done();
-          });
-        })
-        .catch(done);
+      var res = fs.createReadStream(path.join(DATA_DIR, 'fixture.js')).pipe(createWriteStream(TARGET, { progress: progress }));
+      res.on('error', function (err) {
+        assert.ok(!err);
+      });
+      res.on('finish', function () {
+        fs.readdir(TMP_DIR, function (err, files) {
+          assert.ok(!err);
+          assert.equal(files.length, 1);
+          assert.equal(files[0], 'target');
+          assert.ok(progressUpdates.length > 0);
+          done();
+        });
+      });
     });
 
     it('extract tar with progress', function (done) {
@@ -65,9 +69,11 @@ describe('extract', function () {
         progressUpdates.push(update);
       }
 
-      extract(path.join(DATA_DIR, 'fixture.tar'), TARGET, { strip: 1, progress: progress }, function (err) {
+      var res = fs.createReadStream(path.join(DATA_DIR, 'fixture.tar')).pipe(createWriteStream(TARGET, { type: 'tar', strip: 1, progress: progress }));
+      res.on('error', function (err) {
         assert.ok(!err);
-
+      });
+      res.on('finish', function () {
         fs.readdir(TARGET, function (err, files) {
           assert.ok(!err);
           assert.deepEqual(files.sort(), ['file.txt', 'link']);
@@ -79,12 +85,16 @@ describe('extract', function () {
     });
 
     it('extract tar multiple times', function (done) {
-      extract(path.join(DATA_DIR, 'fixture.tar'), TARGET, { strip: 1 }, function (err) {
+      var res = fs.createReadStream(path.join(DATA_DIR, 'fixture.tar')).pipe(createWriteStream(TARGET, { type: 'tar', strip: 1 }));
+      res.on('error', function (err) {
         assert.ok(!err);
-
-        extract(path.join(DATA_DIR, 'fixture.tar'), TARGET, { strip: 1 }, function (err) {
+      });
+      res.on('finish', function () {
+        var res = fs.createReadStream(path.join(DATA_DIR, 'fixture.tar')).pipe(createWriteStream(TARGET, { type: 'tar', strip: 1 }));
+        res.on('error', function (err) {
           assert.ok(!err);
-
+        });
+        res.on('finish', function () {
           fs.readdir(TARGET, function (err, files) {
             assert.ok(!err);
             assert.deepEqual(files.sort(), ['file.txt', 'link']);
@@ -103,9 +113,11 @@ describe('extract', function () {
         progressUpdates.push(update);
       }
 
-      extract(path.join(DATA_DIR, 'fixture.zip'), TARGET, { strip: 1, progress: progress }, function (err) {
+      var res = fs.createReadStream(path.join(DATA_DIR, 'fixture.zip')).pipe(createWriteStream(TARGET, { type: 'zip', strip: 1, progress: progress }));
+      res.on('error', function (err) {
         assert.ok(!err);
-
+      });
+      res.on('finish', function () {
         fs.readdir(TARGET, function (err, files) {
           assert.ok(!err);
           assert.deepEqual(files.sort(), ['file.txt', 'link']);
@@ -119,12 +131,16 @@ describe('extract', function () {
     it('extract zip multiple times', function (done) {
       if (semver.lt(process.versions.node, '0.9.0')) return done(); // yauzl does not read the master record properly on Node 0.8
 
-      extract(path.join(DATA_DIR, 'fixture.zip'), TARGET, { strip: 1 }, function (err) {
+      var res = fs.createReadStream(path.join(DATA_DIR, 'fixture.zip')).pipe(createWriteStream(TARGET, { type: 'zip', strip: 1 }));
+      res.on('error', function (err) {
         assert.ok(!err);
-
-        extract(path.join(DATA_DIR, 'fixture.zip'), TARGET, { strip: 1 }, function (err) {
+      });
+      res.on('finish', function () {
+        var res = fs.createReadStream(path.join(DATA_DIR, 'fixture.zip')).pipe(createWriteStream(TARGET, { type: 'zip', strip: 1 }));
+        res.on('error', function (err) {
           assert.ok(!err);
-
+        });
+        res.on('finish', function () {
           fs.readdir(TARGET, function (err, files) {
             assert.ok(!err);
             assert.deepEqual(files.sort(), ['file.txt', 'link']);
@@ -137,47 +153,27 @@ describe('extract', function () {
   });
 
   describe('unhappy path', function () {
-    it('should fail with too large strip (tar) - path', function (done) {
-      extract(path.join(DATA_DIR, 'fixture.tar'), TARGET, { strip: 2 }, function (err) {
+    it('should fail with too large strip (tar)', function (done) {
+      var res = fs.createReadStream(path.join(DATA_DIR, 'fixture.tar')).pipe(createWriteStream(TARGET, { type: 'tar', strip: 2 }));
+      res.on('error', function (err) {
         assert.ok(!!err);
         done();
       });
-    });
-
-    it('should fail with too large strip (tar) - path - promise', function (done) {
-      if (typeof Promise === 'undefined') return done();
-      extract(path.join(DATA_DIR, 'fixture.tar'), TARGET, { strip: 2 })
-        .then(function () {
-          assert.ok(false);
-        })
-        .catch(function (err) {
-          assert.ok(!!err);
-          done();
-        });
-    });
-
-    it('should fail with too large strip (tar) - stream', function (done) {
-      extract(fs.createReadStream(path.join(DATA_DIR, 'fixture.tar')), TARGET, { type: 'tar', strip: 2 }, function (err) {
-        assert.ok(!!err);
-        done();
+      res.on('finish', function () {
+        assert.ok(false);
       });
     });
 
-    it('should fail with too large strip (zip) - path', function (done) {
+    it('should fail with too large strip (zip)', function (done) {
       if (semver.lt(process.versions.node, '0.9.0')) return done(); // yauzl does not read the master record properly on Node 0.8
 
-      extract(path.join(DATA_DIR, 'fixture.zip'), TARGET, { strip: 2 }, function (err) {
+      var res = fs.createReadStream(path.join(DATA_DIR, 'fixture.zip')).pipe(createWriteStream(TARGET, { type: 'zip', strip: 2 }));
+      res.on('error', function (err) {
         assert.ok(!!err);
         done();
       });
-    });
-
-    it('should fail with too large strip (zip) - stream', function (done) {
-      if (semver.lt(process.versions.node, '0.9.0')) return done(); // yauzl does not read the master record properly on Node 0.8
-
-      extract(fs.createReadStream(path.join(DATA_DIR, 'fixture.zip')), TARGET, { type: 'zip', strip: 2 }, function (err) {
-        assert.ok(!!err);
-        done();
+      res.on('finish', function () {
+        assert.ok(false);
       });
     });
   });
