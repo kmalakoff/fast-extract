97a98fd08a038fd681fcac8682b78b07c729fb55
Kevin Malakoff <kmalakoff@gmail.com>
2020-06-10 18:47:31 -0700

    Update tests for more automation and speed of running

--- test/lib/validateFiles.js
+++ test/lib/validateFiles.js
@@ -7,29 +7,54 @@ var TMP_DIR = path.resolve(path.join(__dirname, '..', '..', '.tmp'));
 var TARGET = path.resolve(path.join(TMP_DIR, 'target'));
 var DATA_DIR = path.resolve(path.join(__dirname, '..', 'data'));
 
-function compareFiles(leftPath, rightPath) {
-  var leftContents = fs.readFileSync(leftPath).toString();
-  var rightContents = fs.readFileSync(rightPath).toString();
-  assert.equal(cr(leftContents), cr(rightContents));
-}
+var fixtureContexts = cr(fs.readFileSync(path.join(DATA_DIR, 'fixture.js')).toString());
 
-module.exports = function validateFiles(files, extractType) {
-  if (extractType === undefined) {
-    assert.equal(files.length, 1);
-    assert.deepEqual(files.sort(), ['target']);
-    compareFiles(path.join(TMP_DIR, files[0]), path.join(DATA_DIR, 'fixture.js'));
-  } else if (extractType === 'js') {
-    assert.equal(files.length, 1);
-    assert.ok(~['fixture.js', 'fixture-js'].indexOf(files[0]));
-    compareFiles(path.join(TARGET, files[0]), path.join(DATA_DIR, 'fixture.js'));
-  } else if (extractType === 'js.gz') {
-    assert.equal(files.length, 1);
-    assert.ok(~['fixture.js.gz', 'fixture-js.gz'].indexOf(files[0]));
-    compareFiles(path.join(TARGET, files[0]), path.join(DATA_DIR, 'fixture.js'));
+module.exports = function validateFiles(options, _type, done) {
+  if (typeof _type === 'function') {
+    done = _type;
+    _type = undefined;
+  }
+  if (typeof options === 'string') options = { type: options };
+  var type = options.type || _type;
+  if (type === undefined) {
+    var dataPath = TMP_DIR;
+    fs.readdir(dataPath, function (err, files) {
+      assert.ok(!err);
+      assert.equal(files.length, 1);
+      assert.deepEqual(files.sort(), ['target']);
+      assert.equal(cr(fs.readFileSync(path.join(dataPath, files[0])).toString()), fixtureContexts);
+      done();
+    });
+  } else if (type === 'js' || type === '.js') {
+    // eslint-disable-next-line no-redeclare
+    var dataPath = TARGET;
+    fs.readdir(dataPath, function (err, files) {
+      assert.ok(!err);
+      assert.equal(files.length, 1);
+      assert.ok(~['fixture.js', 'fixture-js'].indexOf(files[0]));
+      assert.equal(cr(fs.readFileSync(path.join(dataPath, files[0])).toString()), fixtureContexts);
+      done();
+    });
+  } else if (type === 'js.gz' || type === '.js.gz') {
+    // eslint-disable-next-line no-redeclare
+    var dataPath = TARGET;
+    fs.readdir(dataPath, function (err, files) {
+      assert.ok(!err);
+      assert.equal(files.length, 1);
+      assert.ok(~['fixture.js.gz', 'fixture-js.gz'].indexOf(files[0]));
+      assert.equal(cr(fs.readFileSync(path.join(dataPath, files[0])).toString()), fixtureContexts);
+      done();
+    });
   } else {
-    assert.deepEqual(files.sort(), ['fixture.js', 'link']);
-    assert.equal(fs.realpathSync(path.join(TARGET, 'link')), path.join(TARGET, 'fixture.js'));
-    compareFiles(path.join(TARGET, files[0]), path.join(DATA_DIR, 'fixture.js'));
-    compareFiles(path.join(TARGET, files[1]), path.join(DATA_DIR, 'fixture.js'));
+    // eslint-disable-next-line no-redeclare
+    var dataPath = !options.strip ? path.join(TARGET, 'data') : TARGET;
+    fs.readdir(dataPath, function (err, files) {
+      assert.ok(!err);
+      assert.deepEqual(files.sort(), ['fixture.js', 'link']);
+      assert.equal(fs.realpathSync(path.join(dataPath, 'link')), path.join(dataPath, 'fixture.js'));
+      assert.equal(cr(fs.readFileSync(path.join(dataPath, files[0])).toString()), fixtureContexts);
+      assert.equal(cr(fs.readFileSync(path.join(dataPath, files[1])).toString()), fixtureContexts);
+      done();
+    });
   }
 };
