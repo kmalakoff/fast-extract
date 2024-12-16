190bc71051eee7ef389c3ff8af0294d847d48e66
Kevin Malakoff <kmalakoff@gmail.com>
2024-12-15 22:01:51 -0800

    Update queue-cb

--- dist/cjs/optionalRequire.js
+++ dist/cjs/optionalRequire.js
@@ -11,4 +11,4 @@ module.exports = function optionalRequire(name) {
     } catch (_err) {}
     return null;
 };
-/* CJS INTEROP */ if (exports.__esModule && exports.default) { Object.defineProperty(exports.default, '__esModule', { value: true }); for (var key in exports) exports.default[key] = exports[key]; module.exports = exports.default; }
+/* CJS INTEROP */ if (exports.__esModule && exports.default) { try { Object.defineProperty(exports.default, '__esModule', { value: true }); for (var key in exports) { exports.default[key] = exports[key]; } } catch (_) {}; module.exports = exports.default; }
