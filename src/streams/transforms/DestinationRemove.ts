cb827129885dff7b75981e8c7099e63d8180c575
Kevin Malakoff <kmalakoff@gmail.com>
2025-06-28 07:29:00 -0700

    Removed polyfills

--- src/streams/transforms/DestinationRemove.ts
+++ src/streams/transforms/DestinationRemove.ts
@@ -1,11 +1,12 @@
 import rimraf2 from 'rimraf2';
-import { Transform, type TransformCallback, type TransformOptions } from 'stream';
+import type { TransformCallback, TransformOptions, Transform as TransformT } from 'stream';
+import { Transform } from '../../compat/stream.ts';
 
 export default class DestinationRemove extends Transform {
   private dest: string;
   private removed: boolean;
 
-  constructor(dest: string, options: TransformOptions<Transform> = {}) {
+  constructor(dest: string, options: TransformOptions<TransformT> = {}) {
     options = options ? { ...options, objectMode: true } : { objectMode: true };
     super(options);
     this.dest = dest;
